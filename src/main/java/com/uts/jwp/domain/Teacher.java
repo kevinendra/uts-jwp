7b1d7afe072fd7069adec7203420fb1092bbd0b2
kevinendra <64729845+kevinendra@users.noreply.github.com>
2023-12-02 01:35:25 +0700

    update validation

---
+++ src/main/java/com/uts/jwp/domain/Teacher.java
@@ -0,0 +1,55 @@
+package com.uts.jwp.domain;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Teacher {
+ @NotBlank(message = "nip is required")
+    private String nip;
+
+    @NotBlank(message = "full name is required")
+    @Size(min = 3, max = 50)
+    private String fullName;
+
+    @Email(message = "email must @ and is required")
+    @NotBlank(message = "email is required")
+    private String email;
+
+    @NotBlank(message = "phone number is required")
+    private String phoneNumber;
+
+    public Teacher(){
+    }
+
+    public String getNip(){
+        return nip;
+    }
+    public void setNip(String nip){
+        this.nip = nip;
+    }
+
+    
+    public String getFullName(){
+        return fullName;
+    }
+    public void setFullName(String fullName){
+        this.fullName = fullName;
+    }
+
+    
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+}
